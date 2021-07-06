2910ab19023544b1894d3309bba402a7affc12e3
yeungparkhay <yeungparkhay@gmail.com>
2021-07-06 17:50:30 +0100

    updated package.json

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   purge: {
-    enabled: false,
+    enabled: true,
     content: ["./src/**/*.js", "./src/*.js", "./public/index.html"],
   },
   darkMode: false, // or 'media' or 'class'
